4aec86f575dd8595923a03631fb39b7e70913aa0
rhythmm16 <rhythm161203@gmail.com>
2025-11-10 10:43:51 +0530

    FILE UPLOAD

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,11 @@ const path = require('path');
 const {checkSignUp,checkSignIn,checkUser} = require('../middleware/middleware.js');
 const {handleTranslate} = require('../controller/translationController.js');
 const {signUp,signIn} = require('../controller/controller.js');
-
+const {upload, handleFileUpload} = require('../controller/fileUploadController.js');
 
 router.post('/signup',checkSignUp,signUp);
 router.post('/sign-in',checkSignIn,signIn);
 router.post('/translate', checkUser, handleTranslate);
+router.post('/translate-file', checkUser, upload.single('file'), handleFileUpload);
     
-
 module.exports = router;
